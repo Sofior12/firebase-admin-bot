946301ff4ce6d0c917fdd2eebe662b87d49101a6
Sofior12 <163592228+Sofior12@users.noreply.github.com>
2026-08-05 00:48:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import telebot
 import os
 from dotenv import load_dotenv
+from firebase import read_data
 
 load_dotenv()
 
@@ -9,7 +10,28 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "🤖 Firebase Admin Bot Online!")
+    bot.reply_to(
+        message,
+        "🤖 Firebase Admin Bot\n\n"
+        "/read - Read Firebase\n"
+        "/devices - Show devices\n"
+        "/messages - Show messages"
+    )
+
+@bot.message_handler(commands=['read'])
+def read(message):
+    data = read_data()
+    bot.reply_to(message, str(data)[:4000])
+
+@bot.message_handler(commands=['devices'])
+def devices(message):
+    data = read_data("devices")
+    bot.reply_to(message, str(data)[:4000])
+
+@bot.message_handler(commands=['messages'])
+def messages(message):
+    data = read_data("messages")
+    bot.reply_to(message, str(data)[:4000])
 
 print("Bot Started...")
 bot.infinity_polling()
