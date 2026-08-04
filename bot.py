a299c89b1cac2167de89b6641e8d57fc41963a82
Sofior12 <163592228+Sofior12@users.noreply.github.com>
2026-08-05 01:34:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,31 @@
 import telebot
 import os
+import random
 from dotenv import load_dotenv
 from firebase import read_data
-import random
 
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(
         message,
         "🤖 Firebase Admin Bot\n\n"
-        "/read - Read Firebase\n"
-        "/devices - Show devices\n"
-        "/messages - Show messages"
+        "/read - Read Full Firebase\n"
+        "/devices - Random Device\n"
+        "/messages - Random Message"
     )
+
 
 @bot.message_handler(commands=['read'])
 def read(message):
     data = read_data()
     bot.reply_to(message, str(data)[:4000])
+
 
 @bot.message_handler(commands=['devices'])
 def devices(message):
@@ -32,7 +35,6 @@ def devices(message):
         bot.reply_to(message, "No devices found")
         return
 
-    # sirf keys jinke andar dict hai
     keys = [k for k, v in data.items() if isinstance(v, dict)]
 
     if not keys:
@@ -47,3 +49,26 @@ def devices(message):
         f"ID: {device}\n\n"
         f"{data[device]}"
     )
+
+
+@bot.message_handler(commands=['messages'])
+def messages(message):
+    data = read_data("messages")
+
+    if not data:
+        bot.reply_to(message, "No messages found")
+        return
+
+    keys = list(data.keys())
+    msg = random.choice(keys)
+
+    bot.reply_to(
+        message,
+        f"💬 Random Message\n\n"
+        f"ID: {msg}\n\n"
+        f"{data[msg]}"
+    )
+
+
+print("Bot Started...")
+bot.infinity_polling()
